2d528905e647f6759d41ed3392c8141427577fdd
iivqum <joshmcguigan3@gmail.com>
2024-06-22 19:08:38 +0100

    Update timeline.lua

--- lua/fatool/ui/timeline.lua
+++ lua/fatool/ui/timeline.lua
@@ -1,10 +1,3 @@
--- How much the timeline is shifted to the right
-local timeline_left_margin = 32
--- Normal amount of time that can be displayed
-local timeline_display_seconds = 10
--- Maximum time step between timeline divisons
-local timeline_step_seconds = 0.5
-local timeline_top_bar_height = 16
 
 local function closest_multiples(n, factor)
 	--[[
@@ -19,6 +12,13 @@ end
 local PANEL = {}
 
 function PANEL:Init()
+	-- How much the timeline is shifted to the right
+	self.timeline_left_margin = 32
+	-- Normal amount of time that can be displayed
+	self.timeline_display_seconds = 10
+	-- Maximum time step between timeline divisons
+	self.timeline_step_seconds = 0.5
+	self.timeline_top_bar_height = 16	
 	-- How zoomed the timeline is
 	self.scale = 1
 	-- Current sequence to edit
@@ -27,7 +27,7 @@ function PANEL:Init()
 	self:Dock(FILL)
 	
 	self.top_bar = self:Add("DPanel")
-	self.top_bar:SetTall(timeline_top_bar_height)
+	self.top_bar:SetTall(self.timeline_top_bar_height)
 	self.top_bar:Dock(TOP)
 	
 	function self.top_bar:Paint(width, height)	
@@ -36,24 +36,14 @@ function PANEL:Init()
 		--self:DrawFilledRect()
 	end
 	
-	
 	self.top_scroll = self:Add("DScrollPanel")
-	self.top_scroll:DockMargin(timeline_left_margin, 0, 0, 0)
+	self.top_scroll:DockMargin(self.timeline_left_margin, 0, 0, 0)
 	self.top_scroll:Dock(FILL)
 	self.top_scroll:GetCanvas():DockPadding(0, 16, 0, 0)
 	
-	for i = 0, 2 do
-	local test = self.top_scroll:Add("DPanel")
-	test:SetTall(32)
-	test:Dock(TOP)
-	end
-	
 	function self.top_scroll:Paint(width, height)
 		surface.SetDrawColor(150, 150, 150)
-
 		surface.DrawLine(0, 0, 0, height)		
-		--fatool.ui.draw_vertical_dashed_line(4, 10, height, 255, 255, 255)
-		--fatool.ui.draw_horizontal_dashed_line(2, 10, width, 255, 255, 255)
 	end
 	
 	self.bottom_scroll = self:Add("DHScrollBar")
@@ -61,22 +51,29 @@ function PANEL:Init()
 	self.bottom_scroll:SetUp(1, 10)
 end
 
+function PANEL:get_startstop()
+	--[[
+		Purpose:
+			
+	--]]
+end
+
 function PANEL:draw_markers()
 	-- How many markers are on the screen
-	local marker_amount = math.floor(timeline_display_seconds / timeline_step_seconds)
+	local marker_amount = math.floor(self.timeline_display_seconds / self.timeline_step_seconds)
 	-- Distance between markers on the timeline
 	local marker_step = self.top_scroll:GetCanvas():GetWide() / marker_amount
 	-- The time that corresponds to the leftmost border of the timeline
 	local marker_start_time = self.bottom_scroll:GetOffset() * -1
 	-- Closest marker boundaries
-	local marker_lower, marker_upper = closest_multiples(marker_start_time, timeline_step_seconds)
+	local marker_lower, marker_upper = closest_multiples(marker_start_time, self.timeline_step_seconds)
 	-- Where the markers will start from the leftmost border
-	local marker_start_position = (marker_start_time - marker_lower) / timeline_step_seconds * marker_step * -1
+	local marker_start_position = (marker_start_time - marker_lower) / self.timeline_step_seconds * marker_step * -1
 	for i = 0, marker_amount do
-		local marker_number = math.Truncate(marker_lower + timeline_step_seconds * i, 2)
+		local marker_number = math.Truncate(marker_lower + self.timeline_step_seconds * i, 2)
 		marker_number = tostring(marker_number)
-		local marker_x = math.floor(marker_start_position + timeline_left_margin + marker_step * i) 
-		if marker_x >= timeline_left_margin then
+		local marker_x = math.floor(marker_start_position + self.timeline_left_margin + marker_step * i) 
+		if marker_x >= self.timeline_left_margin then
 			local marker_y = self.top_scroll:GetY() - draw.GetFontHeight("DefaultSmall")
 			
 			draw.DrawText(marker_number, "DefaultSmall", marker_x, marker_y, nil, TEXT_ALIGN_CENTER)
